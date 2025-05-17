218440e09b4f9eb435f02b8c115930cb3b021e5d
liklik161 <driesrepriels@outlook.be>
2025-05-17 10:47:38 +0200

    P2L9

--- startcode/food.py
+++ startcode/food.py
@@ -1,6 +1,16 @@
+import pygame
+import random
+from snake import veld_grootte
+kleur_voedsel = (255,0,0)
+
 class Food:
-    def __init__(self):
+    def __init__(self, breedte, hoogte):
+        self.breedte = breedte
+        self.hoogte = hoogte
 
     def plaats_voedsel(self):
+        self.x = round(random.randrange(0, self.breedte - veld_grootte) / veld_grootte) * veld_grootte
+        self.y = round(random.randrange(0, self.hoogte - veld_grootte) / veld_grootte) * veld_grootte
 
     def teken(self, venster):
+        pygame.draw.rect(venster, kleur_voedsel, pygame.Rect(self.x, self.y, veld_grootte, veld_grootte))
--- startcode/main.py
+++ startcode/main.py
@@ -1,17 +1,47 @@
 import pygame
 import time
-from snake import Snake
+from snake import Snake, veld_grootte
 from food import Food
 
 # kleuren
+breedte = 800
+hoogte = 600
+kleur_achtergrond = (0,0,0)
+pygame.init()
+venster = pygame.display.set_mode((breedte, hoogte))
+pygame.display.set_caption("snake")
 
-# schermgrootte
+def game_lus():
+    snake = Snake(breedte / 2, hoogte / 2)
+    game_over = False
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.key == pygame.K_LEFT and snake.x_verandering == 0:
+                snake.x_verandering = -veld_grootte
+                snake.y_verandering = 0
+            elif event.key == pygame.K_RIGHT and snake.x_verandering == 0:
+                snake.x_verandering = veld_grootte
+                snake.y_verandering = 0
+            elif event.key == pygame.K_UP and snake.y_verandering == 0:
+                snake.y_verandering = -veld_grootte
+                snake.x_verandering = 0
+            elif event.key == pygame.K_DOWN and snake.y_verandering == 0:
+                snake.y_verandering = veld_grootte
+                snake.x_verandering = 0
+    snake.beweeg()
+    venster.fill(kleur_achtergrond)
+    snake.teken(venster)
+    pygame.display.update()
+    time.sleep(1/5)
+    pygame.time.wait(5000)
+    game_lus()
 
-# Snelheid van het spel
 
-# Initialiseren van de pygame-module
 
-# Creëer een venster met opgegeven breedte en hoogte
+
+
 
 # Functie om de score op het scherm te tonen
 
